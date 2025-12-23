62afc72cfda752721ae2d04d04ef627b6542b3df
ipgb <igorpintoguedesdebarros2@gmail.com>
2025-12-23 16:19:19 -0300

    Update NR10.jsx

--- src/NR10.jsx
+++ src/NR10.jsx
@@ -5,14 +5,14 @@ const numeroWhatsApp = "+5521920439641";
 /* 👉 CONFIGURAÇÃO DOS CONTEÚDOS */
 const conteudos = {
   pdfs: [
-    { id: 1, titulo: "Apostila 1", arquivo: "/public/IMGs/NR 10 BASICA/_Efeitos da corrente sobre o corpo humano.pdf" },
-    { id: 2, titulo: "Apostila 2", arquivo: "/public/IMGs/NR 10 BASICA/1 Legislação NR 10.pdf" },
-    { id: 3, titulo: "Apostila 3", arquivo: "/public/IMGs/NR 10 BASICA/2 NORMA REGULAMENTADORA 10 ANEXO1.pdf" },
-    { id: 4, titulo: "Apostila 4", arquivo: "/public/IMGs/NR 10 BASICA/3 APOSTILA NR 10.pdf" },
-    { id: 5, titulo: "Apostila 5", arquivo: "/public/IMGs/NR 10 BASICA/4  EPI & EPR-1-1.pdf" },
-    { id: 6, titulo: "Apostila 6", arquivo: "/public/IMGs/NR 10 BASICA/5  Aterramento.pdf" },
-    { id: 7, titulo: "Apostila 7", arquivo: "/public/IMGs/NR 10 BASICA/norma nr 10.pdf" },
-    { id: 8, titulo: "Apostila 8", arquivo: "/public/IMGs/NR 10 BASICA/nr 10.pdf" },
+    { id: 1, titulo: "Apostila 1", arquivo: "/IMGs/NR 10 BASICA/_Efeitos da corrente sobre o corpo humano.pdf" },
+    { id: 2, titulo: "Apostila 2", arquivo: "/IMGs/NR 10 BASICA/1 Legislação NR 10.pdf" },
+    { id: 3, titulo: "Apostila 3", arquivo: "/IMGs/NR 10 BASICA/2 NORMA REGULAMENTADORA 10 ANEXO1.pdf" },
+    { id: 4, titulo: "Apostila 4", arquivo: "/IMGs/NR 10 BASICA/3 APOSTILA NR 10.pdf" },
+    { id: 5, titulo: "Apostila 5", arquivo: "/IMGs/NR 10 BASICA/4  EPI & EPR-1-1.pdf" },
+    { id: 6, titulo: "Apostila 6", arquivo: "/IMGs/NR 10 BASICA/5  Aterramento.pdf" },
+    { id: 7, titulo: "Apostila 7", arquivo: "/IMGs/NR 10 BASICA/norma nr 10.pdf" },
+    { id: 8, titulo: "Apostila 8", arquivo: "/IMGs/NR 10 BASICA/nr 10.pdf" },
   ],
   videos: [],
 };
@@ -88,7 +88,7 @@ export default function AcessoProtegido() {
      <h2>CURSO NR10 - ELETRICIDADE</h2>
 
 <img
-  src="/public/IMGs/10.jpg"
+  src="/IMGs/10.jpg"
   alt="Imagem do curso"
   style={{
     width: "100%",
@@ -147,7 +147,7 @@ export default function AcessoProtegido() {
 
       {/* ---------- LINK FINAL ---------- */}
       {finalizado && (
-  <button onClick={() => window.open("/src/PROVA-NR10", "_blank")}>
+  <button onClick={() => window.open("/PROVA-NR10", "_blank")}>
     ACESSAR PROVA
   </button>
 )}
