01d2ca2a9c94ae3d1fbee4e55554db0e7677bd18
ipgb <igorpintoguedesdebarros2@gmail.com>
2025-12-23 15:28:46 -0300

    Update Pagamento.jsx

--- src/Pagamento.jsx
+++ src/Pagamento.jsx
@@ -1,28 +1,31 @@
 import { useEffect, useState } from "react";
 
 const cursosPadrao = [
-  { id: 1, nome: "Curso NR1", preco: 50, arquivoCurso: "/NR1" },
-  { id: 2, nome: "Curso NR5", preco: 60, arquivoCurso: "/NR5" },
-  { id: 3, nome: "Curso NR6", preco: 80, arquivoCurso: "/NR6" },
-  { id: 4, nome: "Curso NR10", preco: 50, arquivoCurso: "/NR10" },
-  { id: 5, nome: "Curso NR10HV", preco: 60, arquivoCurso: "/NR10HV" },
-  { id: 6, nome: "Curso NR11", preco: 80, arquivoCurso: "/NR11" },
-  { id: 7, nome: "Curso NR12", preco: 50, arquivoCurso: "/NR12" },
-  { id: 8, nome: "Curso NR13", preco: 60, arquivoCurso: "/NR13" },
-  { id: 9, nome: "Curso NR16", preco: 80, arquivoCurso: "/NR16" },
-  { id: 10, nome: "Curso NR17", preco: 50, arquivoCurso: "/NR17" },
-  { id: 11, nome: "Curso NR18", preco: 60, arquivoCurso: "/NR18" },
-  { id: 12, nome: "Curso NR20", preco: 50, arquivoCurso: "/NR20" },
-  { id: 13, nome: "Curso NR23", preco: 60, arquivoCurso: "/NR23" },
-  { id: 14, nome: "Curso NR26", preco: 80, arquivoCurso: "/NR26" },
-  { id: 15, nome: "Curso NR29", preco: 50, arquivoCurso: "/NR29" },
-  { id: 16, nome: "Curso NR30", preco: 60, arquivoCurso: "/NR30" },
-  { id: 17, nome: "Curso NR32", preco: 80, arquivoCurso: "/NR32" },
-  { id: 18, nome: "Curso NR33", preco: 50, arquivoCurso: "/NR33" },
-  { id: 19, nome: "Curso NR34", preco: 60, arquivoCurso: "/NR34" },
-  { id: 20, nome: "Curso NR35", preco: 80, arquivoCurso: "/NR35" },
-  { id: 21, nome: "Curso NR37", preco: 50, arquivoCurso: "/NR37" },
-  { id: 22, nome: "Curso NR38", preco: 60, arquivoCurso: "/NR38" },
+  const cursosPadrao = [
+  { id: 1, nome: "Curso NR1", preco: 50, arquivoCurso: "#/nr1" },
+  { id: 2, nome: "Curso NR5", preco: 60, arquivoCurso: "#/nr5" },
+  { id: 3, nome: "Curso NR6", preco: 80, arquivoCurso: "#/nr6" },
+  { id: 4, nome: "Curso NR10", preco: 50, arquivoCurso: "#/nr10" },
+  { id: 5, nome: "Curso NR10HV", preco: 60, arquivoCurso: "#/nr10hv" },
+  { id: 6, nome: "Curso NR11", preco: 80, arquivoCurso: "#/nr11" },
+  { id: 7, nome: "Curso NR12", preco: 50, arquivoCurso: "#/nr12" },
+  { id: 8, nome: "Curso NR13", preco: 60, arquivoCurso: "#/nr13" },
+  { id: 9, nome: "Curso NR16", preco: 80, arquivoCurso: "#/nr16" },
+  { id: 10, nome: "Curso NR17", preco: 50, arquivoCurso: "#/nr17" },
+  { id: 11, nome: "Curso NR18", preco: 60, arquivoCurso: "#/nr18" },
+  { id: 12, nome: "Curso NR20", preco: 50, arquivoCurso: "#/nr20" },
+  { id: 13, nome: "Curso NR23", preco: 60, arquivoCurso: "#/nr23" },
+  { id: 14, nome: "Curso NR26", preco: 80, arquivoCurso: "#/nr26" },
+  { id: 15, nome: "Curso NR29", preco: 50, arquivoCurso: "#/nr29" },
+  { id: 16, nome: "Curso NR30", preco: 60, arquivoCurso: "#/nr30" },
+  { id: 17, nome: "Curso NR32", preco: 80, arquivoCurso: "#/nr32" },
+  { id: 18, nome: "Curso NR33", preco: 50, arquivoCurso: "#/nr33" },
+  { id: 19, nome: "Curso NR34", preco: 60, arquivoCurso: "#/nr34" },
+  { id: 20, nome: "Curso NR35", preco: 80, arquivoCurso: "#/nr35" },
+  { id: 21, nome: "Curso NR37", preco: 50, arquivoCurso: "#/nr37" },
+  { id: 22, nome: "Curso NR38", preco: 60, arquivoCurso: "#/nr38" },
+];
+
 ];
 
 export default function Cursos() {
