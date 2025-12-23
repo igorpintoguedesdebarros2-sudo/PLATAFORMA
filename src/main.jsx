d62c837a980f32c7923d5d97e469657114814af0
ipgb <igorpintoguedesdebarros2@gmail.com>
2025-12-23 15:32:48 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,10 @@
-import { BrowserRouter } from "react-router-dom";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { HashRouter } from "react-router-dom";
+import App from "./App";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter basename="/PLATAFORMA/">
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <HashRouter>
     <App />
-  </BrowserRouter>
-)
+  </HashRouter>
+);
