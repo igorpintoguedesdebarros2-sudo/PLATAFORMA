f67ef3eee36989ec2f6598f3c056daaf39fad023
ipgb <igorpintoguedesdebarros2@gmail.com>
2025-12-25 13:01:17 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { Routes, Route } from "react-router-dom";
 import Login from "./auth/Login";
 import Register from "./auth/Register";
 import Profile from "./auth/Profile";
-import Home from "./Home"; // antes podia estar ../../../home
+import Home from "./Home";
 import Pagamento from "./Pagamento";
 import Preparatorio from "./Preparatorio";
 
