6be5bb1b1f0ef41ae3a24596655847b3e6680184
ipgb <igorpintoguedesdebarros2@gmail.com>
2025-12-25 12:04:22 -0300

    Update HOME.jsx

--- HOME.jsx
+++ HOME.jsx
@@ -1,48 +1,36 @@
-// src/Index.jsx
 import React from "react";
-import { Link } from "react-router-dom";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
-export default function Index() {
+export default function Home() {
   return (
-    <div>
+    <div className="container mt-5 text-center">
       {/* Dropdown */}
-      <div className="dropdown">
+      <div className="dropdown mb-4">
         <button
           className="btn btn-secondary dropdown-toggle"
           type="button"
           data-bs-toggle="dropdown"
           aria-expanded="false"
         >
-          Dropdown button
+          Menu
         </button>
         <ul className="dropdown-menu">
           <li>
-            <Link className="dropdown-item" to="/profile">
+            <a className="dropdown-item" href="/profile">
               PERFIL
-            </Link>
+            </a>
           </li>
         </ul>
       </div>
 
-      {/* Conteúdo principal */}
-      <h1 style={{ textAlign: "center" }}>
-        VENHA FAZER NOSSOS CURSOS E EMBARQUE COM SEGURANÇA
-      </h1>
-      <p style={{ textAlign: "center" }}>
-        aqui temos diversos cursos de NRs e preparatorios
-      </p>
+      {/* Título e descrição */}
+      <h1 className="primario">VENHA FAZER NOSSOS CURSOS E EMBARQUE COM SEGURANÇA</h1>
+      <p className="secundario">Aqui temos diversos cursos de NRs e preparatórios</p>
 
-      <p>
-        <strong>
-          clique no botão abaixo pra preencher uma planilha pra acessar nosso cursos
-        </strong>
-      </p>
-
-      <Link to="/planilha">
-        <button className="btn btn-primary">ACESSAR</button>
-      </Link>
+      {/* Botão para planilha */}
+      <p><strong>Clique no botão abaixo para preencher uma planilha e acessar nossos cursos</strong></p>
+      <a href="planilha.html" className="btn btn-primary">
+        ACESSAR
+      </a>
     </div>
   );
 }
