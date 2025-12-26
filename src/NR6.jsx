f284fd9e623564030353244817f27f02a302bd72
ipgb <igorpintoguedesdebarros2@gmail.com>
2025-12-26 18:46:13 -0300

    Update NR6.jsx

--- src/NR6.jsx
+++ src/NR6.jsx
@@ -142,7 +142,7 @@ export default function AcessoProtegido() {
 
       {/* ---------- LINK FINAL ---------- */}
       {finalizado && (
-  <button onClick={() => window.open("/PROVA-NR6", "_blank")}>
+  <button onClick={() => window.open("/nr6/prova", "_blank")}>
     ACESSAR PROVA
   </button>
 )}
