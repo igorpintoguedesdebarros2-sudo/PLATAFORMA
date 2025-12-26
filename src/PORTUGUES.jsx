07b434f061f30a16d848a4909feda06f27545579
ipgb <igorpintoguedesdebarros2@gmail.com>
2025-12-26 18:58:58 -0300

    Update PORTUGUES.jsx

--- src/PORTUGUES.jsx
+++ src/PORTUGUES.jsx
@@ -128,7 +128,7 @@ export default function AcessoProtegido() {
 
       {/* ---------- LINK FINAL ---------- */}
       {finalizado && (
-  <button onClick={() => window.open("/PROVA-PORTUGUES", "_blank")}>
+  <button onClick={() => window.open("/portugues/prova", "_blank")}>
     ACESSAR PROVA
   </button>
 )}
