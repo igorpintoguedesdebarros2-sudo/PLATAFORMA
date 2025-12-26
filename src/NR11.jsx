b3b8b72e842fd8fd69b073c22ac021234deaa36a
ipgb <igorpintoguedesdebarros2@gmail.com>
2025-12-26 18:49:27 -0300

    Update NR11.jsx

--- src/NR11.jsx
+++ src/NR11.jsx
@@ -148,7 +148,7 @@ export default function AcessoProtegido() {
 
       {/* ---------- LINK FINAL ---------- */}
       {finalizado && (
-  <button onClick={() => window.open("/PROVA-NR11", "_blank")}>
+  <button onClick={() => window.open("/nr11/prova", "_blank")}>
     ACESSAR PROVA
   </button>
 )}
