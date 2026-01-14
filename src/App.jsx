19169ed3a00fab47f175ed1c4127980fdac712cf
ipgb <igorpintoguedesdebarrospintoguedesdebarros2@gmail.com>
2026-01-14 15:21:37 -0300

    Atualizar site com build do dist

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Login from "./auth/Login";
 import Register from "./auth/Register";
 import Perfil from "./auth/Profile";
@@ -10,16 +10,18 @@ import NR1 from "./NR1";
 
 export default function App() {
   return (
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/perfil" element={<Perfil />} />
-         <Route path="/home" element={<Home />} />
-          <Route path="/planilha" element={<Planilha />} />
-           <Route path="/pagamento" element={<Pagamento />} />
-            <Route path="/preparatorio" element={<Preparatorio />} />
-            <Route path="/nr1" element={<NR1 />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/planilha" element={<Planilha />} />
+        <Route path="/pagamento" element={<Pagamento />} />
+        <Route path="/preparatorio" element={<Preparatorio />} />
+        <Route path="/nr1" element={<NR1 />} />
       </Routes>
+    </HashRouter>
   );
 }
