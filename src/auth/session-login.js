8210ac206c433a90761553cacb61e248b01a9219
ipgb <igorpintoguedesdebarrospintoguedesdebarros2@gmail.com>
2026-04-24 14:22:02 -0300

    add stripe dependency

--- src/auth/session-login.js
+++ src/auth/session-login.js
@@ -29,7 +29,7 @@ app.use((req, res, next) => {
 
 // 🔐 CORS
 app.use(cors({
-  origin: "https://igorpintoguedesdebarros2-sudo.github.io/", // depois você troca
+  origin: "plataforma-production-ea85.up.railway.app", // depois você troca
   credentials: true
 }));
 
