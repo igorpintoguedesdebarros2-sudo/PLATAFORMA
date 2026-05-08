20114e9d46a3f4089f8c06eb31699ce328d735ea
ipgb <igorpintoguedesdebarrospintoguedesdebarros2@gmail.com>
2026-05-08 09:54:50 -0300

    deploy frontend

--- server.js
+++ server.js
@@ -710,8 +710,6 @@ app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "dist", "index.html"));
 });
 
-const PORT = process.env.PORT || 3000;
-
 app.listen(PORT, "0.0.0.0", () => {
   console.log("Servidor rodando na porta", PORT);
 });
