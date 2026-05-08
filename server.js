d021eae60532134250557db160a718347f40e6b5
ipgb <igorpintoguedesdebarrospintoguedesdebarros2@gmail.com>
2026-05-08 12:25:38 -0300

    build vite

--- server.js
+++ server.js
@@ -702,14 +702,16 @@ if (!PORT) {
   throw new Error("PORT não veio do Railway");
 }
 
-// arquivos do React
-app.use(express.static(path.join(__dirname, "dist")));
+/// FRONTEND VITE
+app.use(express.static(path.join(__dirname, "plataforma/dist")));
 
-// fallback React Router
-app.use(express.static(path.join(__dirname, "dist")));
+// React Router fallback
+app.use((req, res) => {
+  res.sendFile(path.join(__dirname, "plataforma/dist/index.html"));
+});
 
-app.get("/*", (req, res) => {
-  res.sendFile(path.join(__dirname, "dist", "index.html"));
+app.listen(PORT, "0.0.0.0", () => {
+  console.log("Servidor rodando na porta", PORT);
 });
 
 setInterval(() => {
